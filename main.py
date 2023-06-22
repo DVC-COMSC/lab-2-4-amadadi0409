8a3b55ad7503e4761b374bab04cdeb8cead568c5
amadadi0409 <adwaithrmadadi@gmail.com>
2023-06-21 22:28:48 -0700

    Assignment Lab4

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    original_str = "Python Programming"
-    sub1 = original_str[0:6]
-    sub2 = original_str[6:18]
-    merge_str = sub1 + sub2
+    original_str = "Python Programming "
+    sub1 = original_str[0:7]
+    sub2 = original_str[7:19]
+    print(sub2)
+    print(sub1)
+    merge_str = sub2 + sub1
     print(merge_str)
 
 
