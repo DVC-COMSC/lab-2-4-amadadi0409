b76fdeeb97a73a005e80f578b93d9c3cd255040f
amadadi0409 <adwaithrmadadi@gmail.com>
2023-06-21 21:52:39 -0700

    Lab 4 Assignment

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    pass
+    original_str = "Python Programming"
+    sub1 = original_str[0:6]
+    sub2 = original_str[6:18]
+    merge_str = sub1 + sub2
+    print(merge_str)
 
 
 if __name__ == '__main__':
